055b76752b186c79bd5e8380dea299878279ef9c
guilherme-alc <guilhermealc01@gmail.com>
2025-05-11 11:32:02 -0300

    Adiciona participante padrão

---
+++ Participante5000/Program.cs
@@ -0,0 +1,36 @@
+
+namespace Participante5000
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.WebHost.UseUrls("http://localhost:5000");
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
